eece6bbe5e9e718500939fd9b2bb9a4bdc5d5edd
JoKlim <klimesch.jonathan@gmail.com>
2020-11-06 12:40:05 +0100

    Added extrapolation for StaggeredGrids.

--- scripts/extrapolation_tests.py
+++ scripts/extrapolation_tests.py
@@ -1,40 +1,46 @@
 from phi.flow import *
+from phi.math._tensors import Tensor
 from functools import partial
 
 
-def extp_cgrid(cgrid: CenteredGrid) -> CenteredGrid:
+def extrapolate(values: Tensor, size: int = 1) -> Tensor:
+    if size == 0:
+        return values
     # extrapolation vertically and horizontally
-    cgrid_l, cgrid_r = math.shift(cgrid.values, (-1, 1))
+    values_l, values_r = math.shift(values, (-1, 1))
     where = partial(math.where, value_true=1, value_false=0)
-    mask = math.sum(where(cgrid_l) + where(cgrid_r), axis='shift')
-    extp = math.divide_no_nan(math.sum(cgrid_l + cgrid_r, axis='shift'), mask)
+    mask = math.sum(where(values_l) + where(values_r), axis='shift')
+    extp = math.divide_no_nan(math.sum(values_l + values_r, axis='shift'), mask)
     # extrapolate diagonally
-    cgrid_ll, cgrid_lr = math.shift(cgrid_l.shift[0], (-1, 1), axes='y')
-    cgrid_rl, cgrid_rr = math.shift(cgrid_r.shift[0], (-1, 1), axes='y')
-    mask = where(cgrid_ll) + where(cgrid_lr) + where(cgrid_rl) + where(cgrid_rr)
-    extp_diag = math.divide_no_nan(cgrid_ll + cgrid_lr + cgrid_rl + cgrid_rr, mask).unstack('shift')[0]
+    values_ll, values_lr = math.shift(values_l.shift[0], (-1, 1), axes='y')
+    values_rl, values_rr = math.shift(values_r.shift[0], (-1, 1), axes='y')
+    mask = where(values_ll) + where(values_lr) + where(values_rl) + where(values_rr)
+    extp_diag = math.divide_no_nan(values_ll + values_lr + values_rl + values_rr, mask).unstack('shift')[0]
     # prioritize results from vertical and horizontal shifting over diagonal shifting
     extp = math.where(extp, extp, extp_diag)
-    return CenteredGrid(math.where(cgrid.values, cgrid.values, extp), bounds=cgrid.bounds, extrapolation=cgrid.extrapolation)
+    return math.where(values, values, extrapolate(extp, size=size-1))
+
+
+def extp_sgrid(sgrid: StaggeredGrid, size: int = 1) -> StaggeredGrid:
+    tensors = []
+    for cgrid in sgrid.unstack('vector'):
+        tensors.append(extrapolate(cgrid.values, size=size))
+    return StaggeredGrid(math.channel_stack(tensors, 'vector'), sgrid.box, sgrid.extrapolation)
 
 
 # domain = Domain((3, 3), boundaries=CLOSED, bounds=Box(0, (3, 3)))
 # positions = math.batch_stack([math.tensor([(1, 1)])], axis='points')
 
-domain = Domain((8, 8), boundaries=CLOSED, bounds=Box(0, (8, 8)))
-positions = math.tensor([(3, 3), (3, 4), (4, 3)], names='points,:')
+domain = Domain((4, 4), boundaries=CLOSED, bounds=Box(0, (4, 4)))
+positions = math.tensor([(1, 1), (1, 2), (2, 1), (2, 2)], names='points,:')
+cloud = PointCloud(Sphere(positions, 1))
+sgrid = cloud.at(domain.sgrid(0))
+cgrid = cloud.at(domain.grid(0))
 
-points = PointCloud(Sphere(positions, 1), values=[1, -1])
+extp = extp_sgrid(sgrid, 1)
 
-cgrid = points.at(domain.grid())
-
-extp = extp_cgrid(cgrid)
-
-print(cgrid.values.numpy())
-print('\n')
-print(extp.values.numpy())
-
-# app = App()
-# app.add_field('cgrid', cgrid)
-# app.add_field('sgrid', sgrid)
-# show(app)
+app = App()
+app.add_field('cgrid', cgrid)
+app.add_field('sgrid', sgrid)
+app.add_field('extp', extp)
+show(app)
